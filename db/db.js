e16f3cf0b8dc50f51c526b58aabf312af4c82e5f
Anthony FS <anthonyfs@Anthonys-MBP.attlocal.net>
2022-11-06 16:01:56 -0800

    Added tests for db functions

--- db/db.js
+++ db/db.js
@@ -1,17 +1,29 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
 const User = require("../api/models/User")
 
+const connect = async (dbName) => {
+    mongoose.connect(process.env.dburl + dbName || "test-db")
+}
+
+const disconnect = async () => {
+    await mongoose.connection.close()
+}
+
 const findUser = async (obj) => {
-    console.log(obj)
     let found = await User.findOne({ email: obj.email }).exec()
-    console.log(found)
     return found
 }
 
 const saveUser = async (user) => {
     await user.save()
+    return user
 }
 
 module.exports = {
     findUser,
-    saveUser
+    saveUser,
+    connect,
+    disconnect
 }
